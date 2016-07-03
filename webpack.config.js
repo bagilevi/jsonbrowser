c8a3a87c130a29ab426951b10a7cf727212dc58b
Levente Bagi <bagilevi@gmail.com>
2016-07-03 11:08:48 +0100

    style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:4949',
     'webpack/hot/only-dev-server',
-    './src/index'
+    './src/index',
+    './style/index'
   ],
   resolve: {
     root: [path.join(__dirname, "src")],
-    extensions: ["", ".webpack.js", ".web.js", ".js", ".ls"]
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".ls", ".css", ".styl"]
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -18,17 +20,29 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
+    new ExtractTextPlugin("style.css", { allChunks: true }),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\/ls$/,
-      loaders: ['livescript'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\/ls$/,
+        loaders: ['livescript'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('stylus-loader', 'css-loader!stylus-loader')
+      }
+    ]
   }
 };
