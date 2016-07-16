511b712910ecff0a958e8caaf64a8c4ad6a364c2
Levente Bagi <bagilevi@gmail.com>
2016-07-16 15:34:02 +0100

    express boilerplate with webpack

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(4949, 'localhost', function (err, result) {
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 4949 : process.env.PORT;
+const app = express();
+
+if (isDeveloping) {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+} else {
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+}
+
+app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
-
-  console.log('Listening at http://localhost:4949/');
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
